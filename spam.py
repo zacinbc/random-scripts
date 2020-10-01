ac1dbc4ba18920cb9a68d9a485e4fb9a921650e7
zachery <zacinbc@outloo.com>
2020-09-30 20:01:10 -0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -4,20 +4,22 @@ randstring = False
 keyboard = Controller()
 time.sleep(2)
 lyrics=[
-    "We're no strangers to love",
-    "You know the rules and so do I",
-    "A full commitment's what I'm thinking of",
-    "You wouldn't get this from any other guy",
-    "I just wanna tell you how I'm feeling",
-    "Gotta make you understand",
-    "Never gonna give you up",
-    "Never gonna let you down",
-    "Never gonna run around and desert you",
-    "Never gonna make you cry",
-    "Never gonna say goodbye",
-    "Never gonna tell a lie and hurt you",
-    "We've known each other for so long",
-    "------------------------"
+    # "We're no strangers to love",
+    # "You know the rules and so do I",
+    # "A full commitment's what I'm thinking of",
+    # "You wouldn't get this from any other guy",
+    # "I just wanna tell you how I'm feeling",
+    # "Gotta make you understand",
+    # "Never gonna give you up",
+    # "Never gonna let you down",
+    # "Never gonna run around and desert you",
+    # "Never gonna make you cry",
+    # "Never gonna say goodbye",
+    # "Never gonna tell a lie and hurt you",
+    # "We've known each other for so long",
+    # "------------------------"
+    "@MUTED (Yea you can ping them as much as you like)"
+
 ]
 while True:
     time.sleep(0.75)
