1d0f22e96b3eda7ea7aed8f021b47a5121ecda21
zachery <zacinbc@outloo.com>
2020-10-28 12:57:33 -0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -4,33 +4,30 @@ randstring = False
 keyboard = Controller()
 time.sleep(2)
 lyrics=[
-    # "We're no strangers to love",
-    # "You know the rules and so do I",
-    # "A full commitment's what I'm thinking of",
-    # "You wouldn't get this from any other guy",
-    # "I just wanna tell you how I'm feeling",
-    # "Gotta make you understand",
-    # "Never gonna give you up",
-    # "Never gonna let you down",
-    # "Never gonna run around and desert you",
-    # "Never gonna make you cry",
-    # "Never gonna say goodbye",
-    # "Never gonna tell a lie and hurt you",
-    # "We've known each other for so long",
-    # "------------------------"
-    "@MUTED (Yea you can ping them as much as you like)"
-
+    "We're no strangers to love",
+    "You know the rules and so do I",
+    "A full commitment's what I'm thinking of",
+    "You wouldn't get this from any other guy",
+    "I just wanna tell you how I'm feeling",
+    "Gotta make you understand",
+    "Never gonna give you up",
+    "Never gonna let you down",
+    "Never gonna run around and desert you",
+    "Never gonna make you cry",
+    "Never gonna say goodbye",
+    "Never gonna tell a lie and hurt you",
+    "We've known each other for so long",
+    "------------------------"
 ]
 while True:
-    time.sleep(0.75)
+    time.sleep(2.5)
     for line in lyrics:
         thingtosend = line
-        # if randstring:
-        #     thingtosend=re.sub(r'(...)', r'\1 ', thingtosend, random.randint(0, len(thingtosend)))
+        if randstring:
+            thingtosend=re.sub(r'(...)', r'\1 ', thingtosend, random.randint(0, len(thingtosend)))
         for char in thingtosend:
             keyboard.press(char)
             time.sleep(0.01)
             keyboard.release(char)
         keyboard.press(Key.enter)
         keyboard.release(Key.enter)
-    
